184ae17c3cf5ba6220fbb0f3aa7444b623c31f39
tandavala <jose.@tandavala.com>
2018-08-01 16:56:34 +0100

    moving root server content to the the server src file

--- rest_api/server.js
+++ rest_api/server.js
@@ -1,10 +1,11 @@
 'use strict'
 
+
+const app = require('../src/app')
 const http = require("http");
 const debug = require("debug")('nodestr:server');
-const express = require('express');
 
-const app = express();
+
 const port = normalizePort(process.env.PORT || '3000');
 
 app.set('port', port);
