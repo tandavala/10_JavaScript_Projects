a5cefc76b5097d584be4db0c79536a7c2cc26a15
tandavala <jose.@tandavala.com>
2018-08-03 12:26:20 +0100

    adding controlers and associating it with routes

--- rest_api/src/routes/index-route.js
+++ rest_api/src/routes/index-route.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({ title: "Node Store API", version: "0.0.1"});
+    res.status(200).send({ title: "Node Store API(prozone)", version: "0.0.1"});
 });
 
 module.exports = router;
